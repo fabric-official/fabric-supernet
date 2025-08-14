6e0dc72f9c6cf2f3025feca957fd023becbf2c0b
fab-project-V1 <fabric_v1@gmail.com>
2025-08-14 12:12:18 -0700

    mint: add zero-compiler runtime façade (+NoBuild) and npm script

---
+++ runtime/mint-runtime.ps1
@@ -0,0 +1,7 @@
+﻿param([switch]$Quiet, [switch]$NoBuild)
+$ErrorActionPreference="Stop"
+$node = (Get-Command node -ErrorAction SilentlyContinue)
+if (-not $node) { throw "Node.js is required in PATH to run runtime/mint-runtime.js" }
+$args = @()
+if ($NoBuild -or $env:SUPERNET_SKIP_BUILD -eq '1') { $args += '--no-build' }
+& node .\runtime\mint-runtime.js @args | ForEach-Object { if(-not $Quiet){ $_ } }
